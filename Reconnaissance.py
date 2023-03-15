5185cc323b803a3745fc8437210bfb9daa090118
PPPPPL <67387603+PPPPLI@users.noreply.github.com>
2023-03-15 23:26:07 +0100

    Add files via upload

---
+++ Reconnaissance.py
@@ -0,0 +1,28 @@
+def reconnaissance_mots(nouveaux_etats, mot, table_transition, etat_sorties):
+
+    #créer une dictionnaire pour indexer chaque état
+    dictionnaire_etat = {}
+    index = 0;
+    buffer = nouveaux_etats[0]
+
+    for each in nouveaux_etats:
+        dictionnaire_etat[each] = index
+        index += 1
+
+    #Parcourir le mot par la table de transition
+    for i in range(len(mot)):
+
+        buffer = table_transition[dictionnaire_etat.get(buffer)][ord(mot[i])-97]
+        if buffer == "P":
+            print("Le mot n'est pas connaissable par l'automate")
+            return
+
+    liste_sortie = []
+    index = 0
+
+    #créer la liste pour stocker les états de sortie
+    for each in etat_sorties:
+        if each == "S":
+            liste_sortie.append(nouveaux_etats[index])
+        index += 1
+    print("Le mot est connaissable par l'automate") if buffer in liste_sortie else print("Le mot n'est pas connaissable par l'automate")
