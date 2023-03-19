ed52e3b2e8177fe142c512b0a5cab1f05fcfbfec
Thomas Raymond <94895516+Thomatoketch@users.noreply.github.com>
2023-03-19 15:20:08 +0100

    correction de bug dans la premiere fonction et creation fonction affichage tableau

--- fonction.py
+++ fonction.py
@@ -1,11 +1,12 @@
 def est_deterministe (table,nbr_initial):
-
+    deterministe = False
+    if nbr_initial > 1:
+        print("Cet automate n'est pas déterministe car il possède plusieurs états initiaux")
     for i in range (len(table)):
         for j in range (len(table[i])):
-            if len(table[i][j])>1:
+            if len(table[i][j])>1 and deterministe == False:
                 print("Cet automate n'est pas déterministe car il ne possède pas au plus 1 transition pour un état ")
-            if nbr_initial > 1:
-                print("Cet automate n'est pas déterministe car il possède plusieurs états initiaux")
+                deterministe = True
 
 
 def est_complet (table):
@@ -18,3 +19,34 @@ def est_complet (table):
         print("cet automate n'est pas complet car il possède des états vides ")
     else:
         print("Cet automate est complet")
+
+
+def affichage_table(nbr_etats,table_entrée_sortie,nbr_initial,nbr_symbole,table_std):
+    print("-------" + "-".join(["------" for i in range(nbr_symbole + 1)]) + "-")
+    print("|{:^5}|{:^6}|".format("E/S", "ETAT"), end="")
+    for k in range(nbr_symbole):
+        letter = chr(k + 97)
+        print(f"{letter:^6}", end="|")
+    print("")
+    print("------+" + "+".join(["------" for i in range(nbr_symbole + 1)]) + "-")
+
+    for g in range(nbr_etats):
+        print(f"|{table_entrée_sortie[g]:^5}", end="| ")
+        if g == nbr_etats and nbr_initial == 1:
+            break
+        if g == nbr_etats:
+            print(f"{'i':^4}", end=' | ')
+            for h in range(nbr_symbole):
+                if table_std[g][h] == "":
+                    print(f"{'--':^4}", end=' | ')
+                else:
+                    print(f"{table_std[g][h]:^4}", end=' | ')
+        else:
+            print(f"{g:^4}", end=' | ')
+            for h in range(nbr_symbole):
+                if table_std[g][h] == "":
+                    print(f"{'--':^4}", end=' | ')
+                else:
+                    print(f"{table_std[g][h]:^4}", end=' | ')
+        print("")
+    print("-------" + "-".join(["------" for i in range(nbr_symbole + 1)]) + "-")
