77c2996d32eebfa13758fa21fa92bc122777c28b
Léa VARING <100589871+lea973@users.noreply.github.com>
2023-03-26 10:51:01 +0200

    correction faute d'orthographe

--- Reconnaissance.py
+++ Reconnaissance.py
@@ -14,12 +14,12 @@ def reconnaissance_mots(nouveaux_etats, mot, table_transition, etat_sorties,nbr_
 
         if (ord(mot[i])-97) >= nbr_symbole or (ord(mot[i])-97) < 0:
 
-            print("Le mot n'est pas connaissable par l'automate")
+            print("Le mot n'est pas reconnu par l'automate")
             return
 
         buffer = table_transition[dictionnaire_etat.get(buffer)][ord(mot[i])-97]
         if buffer == "P":
-            print("Le mot n'est pas connaissable par l'automate")
+            print("Le mot n'est pas reconnu par l'automate")
             return
 
     liste_sortie = []
@@ -30,4 +30,4 @@ def reconnaissance_mots(nouveaux_etats, mot, table_transition, etat_sorties,nbr_
         if each == "S":
             liste_sortie.append(nouveaux_etats[index])
         index += 1
-    print("Le mot est connaissable par l'automate") if buffer in liste_sortie else print("Le mot n'est pas connaissable par l'automate")
+    print("Le mot est reconnu par l'automate") if buffer in liste_sortie else print("Le mot n'est pas reconnu par l'automate")
